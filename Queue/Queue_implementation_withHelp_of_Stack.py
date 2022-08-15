f23e4f158482f218d7f9b33129a778ae4a9ef87f
Avanish Dubey <73096949+avanish7084@users.noreply.github.com>
2022-08-15 10:17:02 +0530

    Update Queue_implementation_withHelp_of_Stack.py

--- Queue/Queue_implementation_withHelp_of_Stack.py
+++ Queue/Queue_implementation_withHelp_of_Stack.py
@@ -1,1 +1,30 @@
 
+class Queue:
+    def __init__(self):
+        self.stack1=[]
+        self.stack2=[]
+
+    def enqueue_operation(self,x):
+        while len(self.stack1)!=0:
+            d=self.stack1.pop()
+            self.stack2.append(d)
+
+        self.stack1.append(x)
+
+        while len(self.stack2)!=0:
+            d=self.stack2.pop()
+            self.stack1.append(d)
+
+    def dequeue_operation(self):
+        if len(self.stack1)==0:
+            print("Queue is empty")
+        while len(self.stack1)!=0:
+            d=self.stack1.pop()
+            print(d)
+
+
+q=Queue()
+q.enqueue_operation(6)
+q.enqueue_operation(5)
+q.dequeue_operation()
+q.dequeue_operation()
