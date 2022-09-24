1fa8bbece16cbc275336ace4092b8dd08ad5281d
Avanish Dubey <73096949+avanish7084@users.noreply.github.com>
2022-09-24 16:20:08 +0530

    Update bubble_sort.py

--- Sorting_Algorithm/bubble_sort.py
+++ Sorting_Algorithm/bubble_sort.py
@@ -1,1 +1,8 @@
 
+arr=list(map(int,input().split()))
+n=len(arr)
+for i in range(n):
+    for j in range(n-i-1):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+print(arr)
