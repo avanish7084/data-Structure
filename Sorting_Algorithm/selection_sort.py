77eac5d12e17ac6b8b338df794ae45e159fec3f3
Avanish Dubey <73096949+avanish7084@users.noreply.github.com>
2022-09-24 17:58:45 +0530

    Update selection_sort.py

--- Sorting_Algorithm/selection_sort.py
+++ Sorting_Algorithm/selection_sort.py
@@ -21,3 +21,21 @@ selection(Array)
 c=time.time()
 print("Sorted array: ",Array)
 print("Time taken",time.time()-c)
+
+
+"""
+
+arr=list(map(int,input().split()))
+n=len(arr)
+for i in range(n):
+    minn=arr[i]
+    loc=i
+    for j in range(i+1,n):
+        if arr[j]<minn:
+            minn=arr[j]
+            loc=j
+    if loc!=i:
+        arr[i],arr[loc]=arr[loc],arr[i]
+print(arr)
+
+"""
